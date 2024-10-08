168973d036b246083c7ea6ae7a64ad2797707e21
arcadie99 <arcadie0307@gmail.com>
2024-10-08 12:33:35 +0200

    small changes and aproach to this dotfiles

---
+++ path_fixer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# zshrc config upload
+ln -fs ~/Dotfiles/zshrc ~/.zshrc
+
+# Config symlink make (nvim at the moment)
+ln -fs ~/Dotfiles/config ~/.config
+
+# tmux config symlink make
+ln -fs ~/Dotfiles/tmux.conf ~/.tmux.conf
+
